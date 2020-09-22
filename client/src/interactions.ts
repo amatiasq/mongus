7ba832ff0b2bbaf00c7491e2db2aabffc89476f3
A. Matías Quezada <amatiasq@gmail.com>
2020-09-22 19:17:42 +0200

    Updated @amatiasq/keyboard to 1.0.1

--- client/src/interactions.ts
+++ client/src/interactions.ts
@@ -1,32 +1,11 @@
-import { KeyboardController, KeyCode } from '@amatiasq/keyboard';
+import { KeyboardActions } from '@amatiasq/keyboard';
+
 import { Action } from '../../shared/Action';
 
-const keyboard = new KeyboardController<Action>();
-const activeActions = new Set<Action>();
+const keyboard = new KeyboardActions<Action>();
 
-const keymaps = {
-  [KeyCode.KeyW]: Action.UP,
-  [KeyCode.KeyS]: Action.DOWN,
-  [KeyCode.KeyA]: Action.LEFT,
-  [KeyCode.KeyD]: Action.RIGHT,
-  [KeyCode.ArrowUp]: Action.UP,
-  [KeyCode.ArrowDown]: Action.DOWN,
-  [KeyCode.ArrowLeft]: Action.LEFT,
-  [KeyCode.ArrowRight]: Action.RIGHT,
-};
+keyboard.setDirections(Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT);
 
 export function watchKeyboard(notify: (actions: Action[]) => void) {
-  Object.entries(keymaps).map(([key, action]) => {
-    keyboard.onKeyDown(key as KeyCode, () => {
-      if (activeActions.has(action)) return;
-      activeActions.add(action);
-      notify(Array.from(activeActions));
-    });
-
-    keyboard.onKeyUp(key as KeyCode, () => {
-      if (!activeActions.has(action)) return;
-      activeActions.delete(action);
-      notify(Array.from(activeActions));
-    });
-  });
+  keyboard.onChange(actions => notify(actions));
 }
